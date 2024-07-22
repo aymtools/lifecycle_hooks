d619336317cc12ca8522aada443f1a053b2de008
aym <anyaming0@gmail.com>
2024-07-22 22:47:01 +0800

    优化调整dispose的时机

--- lib/src/lifecycle_hooks.dart
+++ lib/src/lifecycle_hooks.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:an_lifecycle_cancellable/an_lifecycle_cancellable.dart';
 import 'package:an_lifecycle_viewmodel/an_lifecycle_viewmodel.dart';
@@ -7,8 +8,32 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:weak_collections/weak_collections.dart' as weak;
 
-final Map<BuildContext, LifecycleObserverRegistry> _hooksLifecycleRegistry =
+final Map<BuildContext, _HookLifecycleRegistry> _hooksLifecycleRegistry =
     weak.WeakMap();
+
+class _HookLifecycleRegistry {
+  final Element Function() contextProvider;
+  late final LifecycleObserverRegistryDelegate registry;
+
+  _HookLifecycleRegistry(this.contextProvider) {
+    registry = LifecycleObserverRegistryDelegate(
+        target: registry, contextProvider: contextProvider);
+  }
+
+  LinkedList<_LEntry>? _hooks;
+
+  _LifecycleHookState? get firstOrNullHook =>
+      _hooks == null || _hooks!.isEmpty ? null : _hooks!.first.value;
+
+  _LifecycleHookState? get lastOrNullHook =>
+      _hooks == null || _hooks!.isEmpty ? null : _hooks!.last.value;
+}
+
+class _LEntry extends LinkedListEntry<_LEntry> {
+  _LEntry(this.value);
+
+  _LifecycleHookState value;
+}
 
 class LifecycleHook extends Hook<void> {
   const LifecycleHook();
@@ -17,45 +42,56 @@ class LifecycleHook extends Hook<void> {
   HookState<void, Hook<void>> createState() => _LifecycleHookState();
 }
 
-class _LifecycleHookState extends HookState<void, LifecycleHook>
-    with LifecycleObserverRegistryDelegateMixin {
-  bool _isThis = false;
+class _LifecycleHookState extends HookState<void, LifecycleHook> {
+  BuildContext? _ctx;
 
   @override
   void initHook() {
-    var lifecycle = _hooksLifecycleRegistry[context];
-    if (lifecycle == null) {
-      final ctx = context;
-      _isThis = true;
-      lifecycle = this;
-      _hooksLifecycleRegistry[ctx] = this;
-      addLifecycleObserver(LifecycleObserver.eventDestroy(
-          () => _hooksLifecycleRegistry.remove(ctx)));
-    }
+    var hookLifecycle = _hooksLifecycleRegistry[context];
 
-    if (_isThis) {
-      lifecycleDelegate.initState();
+    if (hookLifecycle == null) {
+      final ctx = context;
+      _ctx = context;
+      hookLifecycle = _HookLifecycleRegistry(() => _ctx as Element);
+      _hooksLifecycleRegistry[ctx] = hookLifecycle;
+      hookLifecycle.registry.addLifecycleObserver(
+          LifecycleObserver.eventDestroy(
+              () => _hooksLifecycleRegistry.remove(ctx)));
+    }
+    hookLifecycle._hooks ??= LinkedList();
+    hookLifecycle._hooks!.add(_LEntry(this));
+
+    if (hookLifecycle.firstOrNullHook == this) {
+      hookLifecycle.registry.initState();
     }
   }
 
   @override
   void build(BuildContext context) {
-    if (_isThis) {
-      lifecycleDelegate.didChangeDependencies();
+    var hookLifecycle = _hooksLifecycleRegistry[context];
+    _ctx = context;
+
+    if (hookLifecycle?.firstOrNullHook == this) {
+      hookLifecycle!.registry.didChangeDependencies();
     }
   }
 
   @override
   void dispose() {
-    if (_isThis) {
-      lifecycleDelegate.dispose();
+    if (_ctx == null) return;
+
+    ///等待最后一个hook销毁时 销毁lifecycle
+    var hookLifecycle = _hooksLifecycleRegistry[_ctx];
+    if (hookLifecycle?.lastOrNullHook == this) {
+      hookLifecycle!.registry.dispose();
     }
+    _ctx = null;
   }
 }
 
 LifecycleObserverRegistry useLifecycle() {
   use(const LifecycleHook());
-  return _hooksLifecycleRegistry[useContext()]!;
+  return _hooksLifecycleRegistry[useContext()]!.registry;
 }
 
 typedef LifecycleEffectTask<T> = FutureOr Function(
